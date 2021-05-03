7b439a7e8d9f4db982aa962cfaf3fb73db812f78
LolingLifeReal <userlessgg@gmail.com>
2021-05-02 21:15:27 -0400

    invite fix

--- commands/invite.js
+++ commands/invite.js
@@ -1,15 +1,22 @@
-
-module.exports={
-  name:'invite',
-  description: "Sends invite",
-  execute(message, args, Discord){
-      const InviteEmbed = new Discord.MessageEvent()
-      .setColour('#a2a5ff')
-      .setTitle('Here is my invite link')
-      .setURL('https://discord.com/oauth2/authorize?client_id=820760544318980096&scope=bot&permissions=2147483647')
-      }
-    };
+module.exports = {
+  name: 'invite',
+  description: 'invites u lmao',
+  execute(message, args) {
+      message.channel.send('https://discord.com/oauth2/authorize?client_id=820760544318980096&scope=bot&permissions=2147483647');
+  }
+}; 
 
 
 
 //https://discord.com/oauth2/authorize?client_id=820760544318980096&scope=bot&permissions=2147483647
+
+
+//name:'invite',
+//description: "Sends invite",
+//execute(message, args, Discord){
+    //const InviteEmbed = new Discord.MessageEvent()
+    //.setColour('#a2a5ff')
+    //.setTitle('Here is my invite link')
+    //.setURL('https://discord.com/oauth2/authorize?client_id=820760544318980096&scope=bot&permissions=2147483647')
+    //}
+  //};
