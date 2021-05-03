d1232857202b8b8b6e8b863d63e7036b4320d170
LolingLifeReal <userlessgg@gmail.com>
2021-05-03 09:14:51 -0400

    kcik

--- commands/kick.js
+++ commands/kick.js
@@ -3,12 +3,17 @@ module.exports = {
     description: "This command kicks a member!",
     execute(message, args){
         const target = message.mentions.users.first();
-        if(target){
-            const memberTarget = message.guild.members.cache.get(target.id);
-            memberTarget.kick();
-            message.channel.send("User has been kicked");
-        }else{
-            message.channel.send(`You coudn't kick that member!`);
+        if (message.content.startsWith(prefix+"kick"))
+        if (message.member.hasPermission("KICK_MEMBERS")) {
+          let member = message.mentions.members.first()
+          if (!member) message.channel.send("Please Mention Someone")
+          else {
+            member.kick().then(mem => {
+              message.channel.send(`Kicked ${mem.user.username}!`)
+            })
+          }
+        } else {
+          message.reply("You Dont Have Permission To Do That!")
         }
-    }
+   }
 }
