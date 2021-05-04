a3854a13116dee52fbdd0f354fe678f552ee9ee7
LolingLifeReal <userlessgg@gmail.com>
2021-05-04 10:49:27 -0400

    ewgwegwegwegweg

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -3,13 +3,12 @@ module.exports = {
     description: "This command shows the info of the server",
     execute(message, args, Discord){
         const embed = new Discord.MessageEmbed()
-            let embed = new Discord.MessageEmbed()
-              .setColor("BLUE")
+              .setColor("Dark violet")
               .setTitle("Server Info")
               .setDescription(`${message.guild}.`)
-              .addField("Member Count", `This Server Has ${message.guild.memberCount} member(s)!`)
-              .addField("Role Count", `This Server Has ${message.guild.roles.cache.size} Role(s)!`)
-              .addField("Emoji Count", `This Server Has ${message.guild.emojis.cache.size} emoji(s)!`)
+              .addField("Member Count", `This Server Has ${message.guild.memberCount} members!`)
+              .addField("Role Count", `This Server Has ${message.guild.roles.cache.size} Roles!`)
+              .addField("Emoji Count", `This Server Has ${message.guild.emojis.cache.size} emoji's!`)
             message.channel.send(embed)
         }
     }
