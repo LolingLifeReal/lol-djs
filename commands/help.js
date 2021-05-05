9c710d7d40ce35f8c5f76e21d8ca02090d667946
LolingLifeReal <userlessgg@gmail.com>
2021-05-04 22:02:24 -0400

    Embeds

--- commands/help.js
+++ commands/help.js
@@ -5,6 +5,20 @@ module.exports= {
         const embed = new Discord.MessageEmbed()
         .setColor("Dark violet")
         .setTitle("Here are the list of commands")
-        .addField
+        .addField("Serverinfo", `>>ServerInfo`)
+        .addField("Ping", `>>Ping`)
+        .addField("Invite", `>>Invite`)
+        .addField("Kick", `>>Kick`)
+        .addField("Ban", `>>Ban`)
+        .addField("Bot Owner's twitter", `>>Twitter`)
+        .addField("Bot Owner's Youtube", `>>Youtube`)
+        .addField("Fat meme", `>>Fat`)
+        .addField("China Meme (Trump)", `>>China`)
+        .addField("Pog", `>>Poggers`)
+        .addField("Serverinfo", `>>ServerInfo`)
+        .addFooter("Commands can be put in uppercase or lower case.")
+
+
+        message.channel.send(embed)
     }
 }
