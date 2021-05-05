74fb09d5451b69379cfab3e993e28e150bcecd3f
LolingLifeReal <userlessgg@gmail.com>
2021-05-05 01:43:37 -0400

    youtube

--- commands/youtube.js
+++ commands/youtube.js
@@ -2,8 +2,7 @@ module.exports = {
     name: 'youtube',
     description: 'This sends loling lifs youtube',
     execute(message, args) {
-        message.channel.send('https://www.youtube.com/channel/UCxu38TeFn3dtlM6fbCsKS7A!');
+        message.channel.send('https://www.youtube.com/channel/UCxu38TeFn3dtlM6fbCsKS7A');
     }
 };
 
-//ODIwNzYwNTQ0MzE4OTgwMDk2.YE520g.0PIUw1uyrZ9P88B13yatJXqXlCY 
