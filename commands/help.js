c1a4cef1431019dd5ea086a256bbe4a5ce58d689
LolingLifeReal <userlessgg@gmail.com>
2021-05-04 21:48:22 -0400

    wefwef

---
+++ commands/help.js
@@ -0,0 +1,10 @@
+module.exports= {
+    name: 'help',
+    description: "This command shows all the comands",
+    execute(message, args, Discord){
+        const embed = new Discord.MessageEmbed()
+        .setColor("Dark violet")
+        .setTitle("Here are the list of commands")
+        .addField
+    }
+}
