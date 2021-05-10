3ae0403d23fcc6ea4061935ad00493af0225570a
LolingLifeReal <userlessgg@gmail.com>
2021-05-10 14:30:06 -0400

    Revert "e"
    This reverts commit ad42bc84726dc27f47142883209f99e8bc5429a0.

---
+++ commands/fat.js
@@ -0,0 +1,7 @@
+module.exports={
+  name:'fat',
+  description: "fat ass",
+  execute(message, args){
+      message.channel.send('https://img-9gag-fun.9cache.com/photo/161230_460s.jpg');
+      }
+    };
