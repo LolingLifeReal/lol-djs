400bcc7b7c88b32eb8f9eade4c8e15ed6ebdeab5
LolingLifeReal <userlessgg@gmail.com>
2021-05-10 14:36:29 -0400

    ewf

--- main.js
+++ main.js
@@ -45,6 +45,8 @@ client.on('message', message =>{
         client.commands.get('ban').execute(message, args)
     }else if (command === 'help'){
         client.commands.get('help').execute(message, args, Discord)
+    }else if (command === 'purge'){
+        client.commands.get('purge').execute(message, args, Discord)
     }
 });
 
